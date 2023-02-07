538da5cf8f65aef0c78b00d80d4393ef0f19d3a1
bensnafz <bensnaf.kc@gmail.com>
2023-02-07 21:57:49 +0700

    commit

---
+++ src/main/java/com/javatechie/springbootmongoaltas/SpringbootMongoAltasApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.springbootmongoaltas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoAltasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoAltasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/springbootmongoaltas/SpringbootMongoAltasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springbootmongoaltas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoAltasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
